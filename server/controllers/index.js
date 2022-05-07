5b1367f8547240f12338ffb92a91acd11866d09f
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-05-07 14:31:22 -0500

    added index and show staff profile

---
+++ server/controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    staff: require('./staff'),
+    patient: require('./patient'),
+    clinic: require('./clinic'),
+}
---
+++ server/controllers/staff.js
@@ -0,0 +1,51 @@
+const db = require('../models');
+
+const index = (req, res) => {
+    let incomingReq = {
+        Staff: req.staffId,
+    }
+    db.Staff.find(incomingReq, (err, foundStaff) => {
+        if (err) {
+            return res
+                .status(400)
+                .json({
+                    message: "Error 400",
+                    err: err,
+                })
+        }
+        return res
+            .status(200)
+            .json({
+                message: "Found Staff",
+                data: foundStaff
+            })
+    })
+}
+
+const show = (req, res) => {
+    db.Staff.findById(req.staffId,
+        (err, foundStaff) => {
+            console.log("FOUND STAFF" + foundStaff)
+            if (err){
+                return res
+                    .status(400)
+                    .json({ 
+                        message: "Failed to find the staff profile",
+                        error:err,
+                    })
+            }   else{
+                console.log(foundStaff)
+                return res
+                    .status(200)
+                    .json({
+                        message: "found staff profile",
+                        data: foundStaff
+                    })
+            }
+        })
+}
+
+module.export = {
+    index, 
+    show
+}
