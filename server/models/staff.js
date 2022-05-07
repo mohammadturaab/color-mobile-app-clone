f086b9726c421d6b5753dfb071fd0cd4a9006a95
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-05-07 13:46:23 -0500

    creates staff schema

---
+++ server/models/staff.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const staffSchema = new Schema ({
+    password: { 
+        type: 'string',
+        select: false,
+    },
+    firstName: String,
+    lastName: String,
+    email: String,
+    admin: {
+        type: 'boolean',
+    },
+    Clinic: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Clinic"
+    }]
+})
