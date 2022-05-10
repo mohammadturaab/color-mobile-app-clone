bf0d0d1e3bd40ab04a27ac28cc5dd840d9588ee1
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-05-10 18:48:15 -0500

    added axios dependency

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -1,8 +1,7 @@
-// setting up config file as an instance of axios, then exporting
+
 import axios from "axios";
 
 const backendAPI = "http://localhost:4000/api";
-//const backendAPI = "https://reaxion.herokuapp.com/api"
 let staff = JSON.parse(localStorage.getItem("staff"))
 
 const client = axios.create({
