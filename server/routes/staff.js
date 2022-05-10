379ca3a00b9958543e57925dfeb3d3140ae3bcac
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-05-10 18:34:09 -0500

    fixing error messages

--- server/routes/staff.js
+++ server/routes/staff.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {staff} = require('../controllers');
-const authRequire = require('../middleware/auth.required')
+const authRequired = require('../middleware/auth.required')
 
 router.get("/", staff.index)
 router.get("/:id", authRequired, staff.show)
