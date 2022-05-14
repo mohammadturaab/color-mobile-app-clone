77a0f14d8363f6cdee42a3c9520becebb2c9b423
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-05-14 10:39:41 -0500

    switching to react

--- client/index.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler';
-import 'react-native-url-polyfill/auto'
-import { registerRootComponent } from 'expo';
-
-import App from './App';
-
-registerRootComponent(App);
--- client/src/api/staff.service.js
+++
@@ -1,8 +0,0 @@
-import client from "./axios.config.js";
-const staff = './staff';
-
-const show = (data, id) => {
-    return client.get(`${staff}/${id}`)
-}
-
-export {show};
